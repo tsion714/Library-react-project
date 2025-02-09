7528c323944ce9b1326475ef88f9d5211b0b518c
Tsion <tsion714@gmail.com>
2025-02-09 18:19:37 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Landing from "./components/Landing";
+import Nav from "./components/Nav";
+import Highlights from "./components/Highlights"
+import Featured from "./components/Featured";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Featured.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+const Featured = () => {
+    return(
+        <section className="features">
+            
+        </section>
+    );
+}
+
+export default Featured;
